30858291f4d551b9cdb2d6c85c453945fe4457f8
KIM JIN SOL <gaussian1137@gmail.com>
2020-01-26 12:44:36 +0900

    update

---
+++ mobilenet_v2/mobilenet_v2.py
@@ -0,0 +1,151 @@
+import torch.nn as nn
+import math
+
+# 3 x 3 convolution + BN + ReLU6
+def conv_bn(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU6(inplace=True)
+    )
+
+# 1 x 1 convolution + BN + ReLU6
+def conv_1x1_bn(inp, oup):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU6(inplace=True)
+    )
+
+
+def make_divisible(x, divisible_by=8):
+    import numpy as np
+    return int(np.ceil(x * 1. / divisible_by) * divisible_by)
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        self.stride = stride
+        # stride는 반드시 1 또는 2이어야 하므로 조건을 걸어 둡니다.
+        assert stride in [1, 2]
+
+        # expansion factor를 이용하여 channel을 확장합니다.
+        hidden_dim = int(inp * expand_ratio)
+
+        # stride가 1인 경우에만 residual block을 사용합니다.
+        # skip connection을 사용하는 경우 input과 output의 크기가 같아야 합니다.
+        self.use_res_connect = (self.stride == 1) and (inp == oup)
+
+        # Inverted Residual 연산
+        self.conv = nn.Sequential(
+            # point-wise convolution
+            nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU6(inplace=True),
+            # depth-wise convolution
+            nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+            nn.BatchNorm2d(hidden_dim),
+            nn.ReLU6(inplace=True),
+            # point-wise linear convolution
+            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(oup),
+        )
+
+    def forward(self, x):
+        # use_res_connect인 경우만 connection을 연결합니다.
+        # use_res_connect : stride가 1이고 input과 output의 채널 수가 같은 경우 True
+        if self.use_res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, n_class=1000, input_size=224, width_mult=1.):
+        super(MobileNetV2, self).__init__()
+        block = InvertedResidual
+        input_channel = 32
+        last_channel = 1280
+        # t : expansion factor
+        # c : output channel의 수
+        # n : 반복 횟수
+        # s : stride
+        interverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+
+        # building first layer
+        assert input_size % 32 == 0
+        # input_channel = make_divisible(input_channel * width_mult)  # first channel is always 32!
+        self.last_channel = make_divisible(last_channel * width_mult) if width_mult > 1.0 else last_channel
+        self.features = [conv_bn(3, input_channel, 2)]
+        
+        # Inverted Residual Block을 생성합니다.
+        # features에 feature들의 정보를 차례대로 저장합니다.
+        for t, c, n, s in interverted_residual_setting:
+            # width multiplier는 layer의 채널 수를 일정 비율로 줄이는 역할을 합니다.
+            output_channel = make_divisible(c * width_mult) if t > 1 else c
+            for i in range(n):
+                if i == 0:
+                    self.features.append(block(input_channel, output_channel, s, expand_ratio=t))
+                else:
+                    self.features.append(block(input_channel, output_channel, 1, expand_ratio=t))
+                input_channel = output_channel
+        
+        # building last several layers
+        self.features.append(conv_1x1_bn(input_channel, self.last_channel))
+        
+        # make it nn.Sequential
+        self.features = nn.Sequential(*self.features)
+
+        # building classifier
+        self.classifier = nn.Linear(self.last_channel, n_class)
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.mean(3).mean(2)
+        x = self.classifier(x)
+        return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+def mobilenet_v2(pretrained=True):
+    model = MobileNetV2(width_mult=1)
+
+    if pretrained:
+        try:
+            from torch.hub import load_state_dict_from_url
+        except ImportError:
+            from torch.utils.model_zoo import load_url as load_state_dict_from_url
+        state_dict = load_state_dict_from_url(
+            'https://www.dropbox.com/s/47tyzpofuuyyv1b/mobilenetv2_1.0-f2a8633.pth.tar?dl=1', progress=True)
+        model.load_state_dict(state_dict)
+    return model
+
+
+if __name__ == '__main__':
+    net = mobilenet_v2(True)
--- readme.md
+++ readme.md
@@ -11,12 +11,14 @@
 <br>
 
 - [mobilenet_v1](https://github.com/gaussian37/Deep-Learning-Implementation/tree/master/mobilenet_v1)
-- mobilenet_v2
+- [mobilenet_v2]((https://github.com/gaussian37/Deep-Learning-Implementation/tree/master/mobilenet_v2))
 - [squeezenet](https://github.com/gaussian37/Deep-Learning-Implementation/tree/master/squeezenet)
 - squeezenext
 - shufflenet
 - efficientnet
 - [resnet](https://github.com/gaussian37/Deep-Learning-Implementation/blob/master/resnet)
+    - resnet with bottleneck
+	- dilated residual network
 
 
 <br>
